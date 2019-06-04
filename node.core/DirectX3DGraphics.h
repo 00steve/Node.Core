191babcf37d93d6e9032a28f4c8b1b01fc291b5e
00steve <stevemkrenz@gmail.com>
2019-06-04 17:39:14 -0500

    Added back as much stuff as before, but I think in the correct way? I guess we'll find out when I finally start using this shit.

--- node.core/DirectX3DGraphics.h
+++ node.core/DirectX3DGraphics.h
@@ -1,7 +1,10 @@
 #pragma once
 
+#define NOMINMAX
+
 #include <wrl.h>
 using namespace Microsoft::WRL;
+
 
 // DirectX 12 specific headers.
 #include <d3d12.h>
@@ -13,7 +16,10 @@ using namespace Microsoft::WRL;
 #include "d3dx12.h"
 #include <exception>
 
-
+// STL Headers
+#include <algorithm>
+#include <cassert>
+#include <chrono>
 
 
 //include project headers
@@ -61,9 +67,31 @@ private:
 	// Can be toggled with the Alt+Enter or F11
 	bool g_Fullscreen = false;
 
+	bool CheckTearingSupport();
+
+	ComPtr<ID3D12CommandAllocator> CreateCommandAllocator(ComPtr<ID3D12Device2> device, D3D12_COMMAND_LIST_TYPE type);
+
+	ComPtr<ID3D12GraphicsCommandList> CreateCommandList(ComPtr<ID3D12Device2> device, ComPtr<ID3D12CommandAllocator> commandAllocator, D3D12_COMMAND_LIST_TYPE type);
+
+	ComPtr<ID3D12CommandQueue> CreateCommandQueue(ComPtr<ID3D12Device2> device, D3D12_COMMAND_LIST_TYPE type);
+
+	ComPtr<ID3D12DescriptorHeap> CreateDescriptorHeap(ComPtr<ID3D12Device2> device, D3D12_DESCRIPTOR_HEAP_TYPE type, uint32_t numDescriptors);
+
+	ComPtr<ID3D12Device2> CreateDevice(ComPtr<IDXGIAdapter4> adapter);
+
+	HANDLE CreateEventHandle();
+
+	ComPtr<ID3D12Fence> CreateFence(ComPtr<ID3D12Device2> device);
+
+	ComPtr<IDXGISwapChain4> CreateSwapChain(HWND hWnd, ComPtr<ID3D12CommandQueue> commandQueue, uint32_t width, uint32_t height, uint32_t bufferCount);
+
 	ComPtr<IDXGIAdapter4> GetAdapter(bool useWarp);
 
+	uint64_t Signal(ComPtr<ID3D12CommandQueue> commandQueue, ComPtr<ID3D12Fence> fence, uint64_t& fenceValue);
 
+	void UpdateRenderTargetViews(ComPtr<ID3D12Device2> device, ComPtr<IDXGISwapChain4> swapChain, ComPtr<ID3D12DescriptorHeap> descriptorHeap);
+
+	void WaitForFenceValue(ComPtr<ID3D12Fence> fence, uint64_t fenceValue, HANDLE fenceEvent, std::chrono::milliseconds duration);
 public:
 	DirectX3DGraphics();
 	~DirectX3DGraphics();
